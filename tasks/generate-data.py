1c6633a1be7a0bc2bfcbcb0127d010c5b68aee70
ccalobeto <calobeto.leon@gmail.com>
2025-02-04 18:16:48 -0500

    add cooking python scripts

---
+++ tasks/generate-data.py
@@ -0,0 +1,125 @@
+# This script generates a csv cleaned file with accumulated data.
+import pandas as pd
+import numpy as np
+import unidecode
+import json
+import topojson as tp
+import geopandas as gpd
+from shapely.geometry import Point, box
+
+
+# The path is different in python in comparison with JS: it is the project path
+FILE = "./src/data/input/mdi_personas_desaparecidas_2017-2023.csv"
+FILE1 = "./src/data/input/mdi_personas_desaparecidas_2024_enero-octubre.csv"
+topo_file = "./src/data/support/ecuador-tm-50k.json"
+
+OUTPUT_LONG_DATA = "./src/data/output/long_data.csv"
+'./src/lib/data/output/long_data.csv'
+OUTPUT_LONG_DATA_WITH_CANTONID = "./src/data/output/long_data_with_cantonId.csv"
+
+df = pd.read_csv(FILE)
+df1 = pd.read_csv(FILE1)
+
+# rename columns
+newColumns = {
+  'edad_aproximada': 'edad',
+  'motivo_desaparcion': 'motivo_desaparicion',
+  'motivo_desaparcion_observada': 'motivo_desaparicion_obs',
+}
+newColumns1 = {
+  'latitud_desaparicion': 'latitud',
+  'longitud_desaparicion': 'longitud',
+  'edad_aproximada': 'edad',
+  'motivo_desaparcion': 'motivo_desaparicion',
+  'motivo_desaparcion_observada': 'motivo_desaparicion_obs',
+}
+df.rename(columns=newColumns, inplace=True)
+df1.rename(columns=newColumns1, inplace=True)
+
+# reorder columns for df1
+df1 = df1[['provincia', 'latitud', 'longitud', 'edad', 'sexo', 'motivo_desaparicion', 'motivo_desaparicion_obs', 'fecha_desaparicion', 'situacion_actual', 'fecha_localizacion', 'rango_edad', 'latitud_encontrado', 'longitud_encontrado']]
+
+# reemplace SIN_DATO with NaN
+df.replace('SIN_DATO', np.nan, inplace=True)
+df1.replace('SIN_DATO', np.nan, inplace=True)
+# do some cleaning and transform. Only float accept nan values
+df['edad'] = df['edad'].astype('float32')
+df['motivo_desaparicion_obs'] = df['motivo_desaparicion_obs'].str.upper()
+df1['edad'] = df1['edad'].astype('float32')
+df1['motivo_desaparicion_obs'] = df1['motivo_desaparicion_obs'].str.upper()
+df1['rango_edad'] = df1['rango_edad'].str.upper().map({'PERSONAS DE LA TERCERA EDAD': 'TERCERA EDAD'})
+df1['latitud_encontrado'] = df1['latitud_encontrado'].astype('float32')
+df1['longitud_encontrado'] = df1['longitud_encontrado'].astype('float32')
+
+## add categories to df
+bins = [-1, 12, 17, 24, 35, 50, 64, np.inf]
+labels = ['NIÑOS', 'ADOLESCENTES', 'JÓVENES ADULTOS', 'ADULTOS JÓVENES', 'ADULTOS', 'ADULTOS MAYORES', 'TERCERA EDAD']
+df['rango_edad'] = pd.cut(df['edad'], bins, labels=labels)
+
+# transform to string date to date
+df['fecha_desaparicion'] = pd.to_datetime(df['fecha_desaparicion'], format='%d/%m/%Y')
+df['fecha_localizacion'] = pd.to_datetime(df['fecha_localizacion'], format='%d/%m/%Y')
+df1['fecha_desaparicion'] = pd.to_datetime(df1['fecha_desaparicion'], format='%d/%m/%Y')
+df1['fecha_localizacion'] = pd.to_datetime(df1['fecha_localizacion'], format='%d/%m/%Y')
+
+# Append dfs
+df2 = pd.concat([df, df1], ignore_index=True)
+
+# fill missing values of edadwith the mean
+meanAgeOfYoungAdults = df2[df2['rango_edad']=='ADULTOS JÓVENES']['edad'].mean()
+df2.loc[df2['edad'].isna(), 'edad'] = meanAgeOfYoungAdults
+# fill missing values of rango_edad
+df2.loc[df2['rango_edad'].isna(),'rango_edad'] = pd.cut(df2['edad'], bins, labels=labels)
+
+# add extra columns for analysis
+df2['disappearance_year'] = df2['fecha_desaparicion'].dt.year
+df2['disappearance_month'] = df2['fecha_desaparicion'].dt.month
+df2['disappearance_day'] = df2['fecha_desaparicion'].dt.day
+df2['days_gone'] = (df2['fecha_localizacion'] - df2['fecha_desaparicion']).dt.days
+# add country ISO 3166-1 A-3 code
+df2['country'] = 'ECU'
+
+# remove accents
+cols_accents_to_remove = ['motivo_desaparicion', 'motivo_desaparicion_obs', 'rango_edad']
+for column in cols_accents_to_remove:
+  df2[column] = df2[column].apply(lambda x: unidecode.unidecode(x) if x is not np.nan else x)
+
+## detect the cantonId from coordinates
+# load cartography canton and convert to geopandas
+with open(topo_file) as f:
+  carto_load = json.load(f)
+canton_topo = tp.Topology(carto_load, object_name='level3')
+canton_dict = json.loads(canton_topo.to_geojson())
+canton_gdf = gpd.GeoDataFrame.from_features(canton_dict['features'])
+
+# convert bbox into a polygon
+geom = box(*canton_gdf.total_bounds)
+
+# prepare the coordinates and covert to geopandas
+df2['coordinates'] = list(zip(df2['longitud'], df2['latitud']))
+df2['coordinates'] = df2['coordinates'].apply(Point)
+# check if the coordinates are inside Ecuador
+df2['in_bounds'] = df2.apply(lambda x: geom.contains(x['coordinates']), axis=1)
+df2_gdf = gpd.GeoDataFrame(df2, geometry='coordinates')
+
+# https://autogis-site.readthedocs.io/en/2019/notebooks/L3/spatial_index.html
+# spatial join
+sjoin = gpd.sjoin(df2_gdf, canton_gdf, how='left')
+df3 = pd.DataFrame(sjoin)
+
+# imputation
+df3['observation'] = 'no'
+df3.loc[(df3['id'].isna() & df3['in_bounds']), 'observation'] = 'imputed with closer cantonId'
+df3['index'] = df3.index
+df3.sort_values(['provincia', 'latitud', 'longitud', 'name'], ascending=[True, True, True, True], inplace=True, ignore_index=True)
+df3[['id', 'name']] = df3[['id', 'name']].fillna(method='ffill')
+# not impute the rows out of bounds
+df3.loc[~df3['in_bounds'],['id', 'name']] = np.nan
+df3.sort_values('index', inplace=True)
+
+# remove unused columns
+df3.drop(columns=['coordinates','index_right', 'index'], inplace=True)
+
+# save files
+df2.to_csv(OUTPUT_LONG_DATA, index=False)
+df3.to_csv(OUTPUT_LONG_DATA_WITH_CANTONID, index=False)
---
+++ tasks/summarize-data.py
@@ -0,0 +1,86 @@
+# This script generates a json file with all data summaries related to the svelte project
+import pandas as pd
+import json
+
+# output json file
+OUTPUT_PATH = "./src/data/"
+OUTPUT_JSON = 'summaries.json'
+ 
+# The path is different in python in comparison with JS: it is the project path
+FILE = "./src/data/output/long_data_with_cantonId.csv"
+FILE1 = "./src/data/support/age_range.csv"
+FILE2 = "./src/data/support/proyeccion_cantonal_total_2010-2020.csv"
+FILE3 = "./src/data/support/categories.csv"
+
+df = pd.read_csv(FILE, dtype={'id': 'str'})
+df1 = pd.read_csv(FILE1)
+df2 = pd.read_csv(FILE2, dtype={'Codigo': 'str'})
+df3 = pd.read_csv(FILE3)
+
+## fill data for missing years and unpivot projection population
+maxYearData = df['disappearance_year'].max()
+years = df2.filter(like='20').columns.to_list()
+years = [int(x) for x in years]
+maxYearProjection = max(years)
+missingYears = list(range(maxYearProjection + 1, maxYearData + 1))
+df2.columns = [x.lower() for x in df2.columns]
+df2.rename(columns=lambda x: 'P' + x if x.startswith('20') else x, inplace=True)
+df2.rename(columns={'codigo': 'cantonId', 'nombre de canton': 'cantonName'}, inplace=True)
+for i in missingYears:
+  df2['P' + str(i)] = df2['P' + str(maxYearProjection)]
+df2 = pd.wide_to_long(df2, stubnames='P', i=['cantonId', 'cantonName'], j='year').reset_index()
+df2.rename(columns={'P': 'proj_population'}, inplace=True)
+
+# join
+all_data = pd.merge(df, df3, on='motivo_desaparicion_obs', how='left')
+
+# aggregations
+agg_by_age_range = all_data.groupby(['country', 'rango_edad']).size().reset_index(name='count')
+agg_by_age_range['percentage'] = 100 * (agg_by_age_range['count'] / agg_by_age_range['count'].sum())
+agg_by_age_range.rename(columns={'rango_edad': 'cardinality'}, inplace=True)
+
+agg_by_year = all_data.groupby(['country', 'disappearance_year']).size().reset_index(name='count')
+agg_by_year['percentage'] = 100 * (agg_by_year['count'] / agg_by_year['count'].sum())
+agg_by_year['disappearance_year'] = agg_by_year['disappearance_year'].astype(str)
+agg_by_year.rename(columns={'disappearance_year': 'cardinality'}, inplace=True)
+
+agg_by_tipology = all_data.groupby(['country', 'tipology']).size().reset_index(name='count')
+agg_by_tipology['percentage'] = 100 * (agg_by_tipology['count'] / agg_by_tipology['count'].sum())
+agg_by_tipology.rename(columns={'tipology': 'cardinality'}, inplace=True)
+
+agg_by_category = all_data.groupby(['country', 'category']).size().reset_index(name='count')
+agg_by_category['percentage'] = 100 * (agg_by_category['count'] / agg_by_category['count'].sum())
+agg_by_category.rename(columns={'category': 'cardinality'}, inplace=True)
+
+agg_by_sex = all_data.groupby(['country', 'sexo']).size().reset_index(name='count')
+agg_by_sex['percentage'] = 100 * (agg_by_sex['count'] / agg_by_sex['count'].sum())
+agg_by_sex.rename(columns={'sexo': 'cardinality'}, inplace=True)
+
+agg_by_canton = all_data.groupby(['country', 'disappearance_year','id', 'name']).size().reset_index(name='missed')
+agg_by_canton = agg_by_canton.merge(df2, how='left', left_on=['id', 'disappearance_year'],right_on=['cantonId', 'year'])
+agg_by_canton['missedPer10k'] = agg_by_canton['missed'] / agg_by_canton['proj_population'] * 10000
+agg_by_canton.drop(columns=['proj_population', 'cantonId', 'cantonName'], inplace=True) 
+# agg_by_canton.rename(columns={'id': 'cantonId'}, inplace=True) 
+
+# convert multidataframes to json
+map_summaries = {'age_range': agg_by_age_range, 
+                 'disappearance_year': agg_by_year, 
+                 'tipology': agg_by_tipology, 
+                 'category': agg_by_category, 
+                 'sex': agg_by_sex,
+                 'canton': agg_by_canton
+                 }
+summaries = []
+for k,v in map_summaries.items():
+  row_summary = {}
+  column = v.columns.to_list()[1]
+  row_summary['kpi'] = k
+  # this is the key. to_json method return str so you have to reconvert to a list with json.loads. finally when you dump it doesn't display with "\"
+  row_summary['data'] = json.loads(v.to_json(orient='records'))
+  summaries.append(row_summary)
+
+with open(OUTPUT_PATH + OUTPUT_JSON, 'w') as f:
+  json.dump(summaries, f)
+
+# agg_by_canton.to_csv(OUTPUT_PATH + 'canton.csv', index=False)
+print('JSON file created.')
