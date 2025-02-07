63e2fc26eaf5ff4c55776b026d8a84f366e50792
ccalobeto <calobeto.leon@gmail.com>
2025-02-07 11:25:54 -0500

    add manager charts file

---
+++ src/components/missing/Charts.svelte
@@ -0,0 +1,25 @@
+<script>
+	import DonutChart from "./DonutChart.svelte";
+	import BarChart from "./BarChart.svelte";
+	import ChoroplethMap from "./ChoroplethMap.svelte";
+
+	export let step;
+	export let scrollyTeller;
+</script>
+
+<div class="charts">
+	{console.log("Charts: step => ", step)}
+	{#each scrollyTeller as item, index}
+		<div class="type-{item['type']}">
+			{#if item.type === "donut-chart"}
+				<DonutChart {step} />
+			{/if}
+			{#if item.type === "bar-chart"}
+				<!-- <BarChart {step} /> -->
+			{/if}
+			{#if item.type === "choropleth-map"}
+				<!-- <ChoroplethMap {step} /> -->
+			{/if}
+		</div>
+	{/each}
+</div>
