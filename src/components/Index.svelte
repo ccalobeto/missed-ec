99426df6a6a298a09517cdbcfed6eef7cceee6cc
ccalobeto <calobeto.leon@gmail.com>
2025-02-27 20:24:38 -0500

    move brief description

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -6,6 +6,12 @@
 	const copy = getContext("copy");
 </script>
 
+<section>
+	<h1>{copy.title}</h1>
+	<p>{@html copy.description}</p>
+	<img src="missing-child.jpg" alt="" />
+</section>
+
 <article>
 	<Missed {copy} />
 </article>
@@ -16,5 +22,30 @@
 	article {
 		display: flex;
 		flex-direction: column;
+	}
+	section {
+		padding: 5px;
+		margin: 5px;
+		box-sizing: border-box;
+	}
+	section h1 {
+		font-family: var(--serif);
+		font-weight: 300;
+		font-size: var(--36px);
+		text-align: center;
+	}
+
+	section p {
+		font-size: var(--14px);
+		font-style: italic;
+		text-align: center;
+		max-width: 500px;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	section img {
+		aspect-ratio: 1;
+		margin: auto;
 	}
 </style>
--- src/components/Missed.svelte
+++ src/components/Missed.svelte
@@ -2,16 +2,9 @@
 	import DonutChart from "./missing/DonutChart.svelte";
 	import BarChart from "./missing/BarChart.svelte";
 	import ChoroplethMap from "./missing/ChoroplethMap.svelte";
-	import { style } from "d3";
 
 	export let copy;
 </script>
-
-<section>
-	<h1>{copy.title}</h1>
-	<p>{@html copy.description}</p>
-	<img src="missing-child.jpg" alt="" />
-</section>
 
 <div class="charts">
 	<!-- {console.log("Charts: step => ", step)} -->
@@ -37,32 +30,6 @@
 </div>
 
 <style>
-	section {
-		padding: 5px;
-		margin: 5px;
-		box-sizing: border-box;
-	}
-	section h1 {
-		font-family: var(--serif);
-		font-weight: 300;
-		font-size: var(--36px);
-		text-align: center;
-	}
-
-	section p {
-		font-size: var(--14px);
-		font-style: italic;
-		text-align: center;
-		max-width: 500px;
-		margin-left: auto;
-		margin-right: auto;
-	}
-
-	section img {
-		aspect-ratio: 1;
-		margin: auto;
-	}
-
 	.text-blocks {
 		font-family: var(--serif);
 		font-size: var(--20px);
